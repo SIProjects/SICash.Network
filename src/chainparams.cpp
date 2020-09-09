ed4ef916d9cf174d8e44e180f03a114992a2eec0
Vidaru <vidaru@protonmail.com>
2020-09-09 14:42:55 -0500

    Updating the seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -243,7 +243,8 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet.sicash.network"); // SICash mainnet
+        vSeeds.emplace_back("testnet.sicash.network"); // SICash testnet
+        vSeeds.emplace_back("testnet.sicash.seeds.vidaru.org"); // Vidaru
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,65);     // Starts with uppercase (T)
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,127);    //Starts with lowercase (t)
